b714b9d5fb6ef2102087b455b56c224206ef14e8
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2022-09-08 22:30:08 +0900

    fizz buzz

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     fizz_buzz(101);
     fizz_buzz_2(101);
+    fizz_buzz_3(101);
 }
 
 fn fizz_buzz(n: i32) {
@@ -20,12 +21,34 @@ fn fizz_buzz(n: i32) {
 }
 
 fn fizz_buzz_2(end: i32) {
-    for n in 1..end {
+    for n in 1..=end {
         match n % 15 {
             n if n % 15 == 0 => println!("FizzBuzz"),
             n if n % 3 == 0 => println!("Fizz"),
             n if n % 5 == 0 => println!("Buzz"),
             _ => println!("{}", n),
+        }
+    }
+}
+
+fn fizz_buzz_3(end: i32) {
+    for x in 1..=end {
+        match x % 15 {
+            0 => println!("FizzBuzz"),
+            3 | 6 | 9 | 12 => println!("Fizz"),
+            5 | 10 => println!("Buzz"),
+            _ => println!("{}", x),
+        }
+    }
+}
+
+fn fizz_buzz_4(end: i32) {
+    for x in 1..=end {
+        match (x % 3, x % 5) {
+            (0, 0) => println!("FizzBuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
+            _ => println!("{}", x),
         }
     }
 }
